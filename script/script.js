b0c85223f2e6751f8d5fa9c70afcaa10e73b8032
Lazaro <Lazaro.Perez0823@gmail.com>
2019-10-24 21:32:12 -0400

    added map

--- script/script.js
+++ script/script.js
@@ -6,13 +6,8 @@ $(document).ready(function () {
     "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
     "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
     "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-
-
-
-
-
-
-
+    
+    
 
 
 
