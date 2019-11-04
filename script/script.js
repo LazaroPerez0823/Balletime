e578eb5a598e81fe999df2257b6fbf9e28dbecab
LAZARO PEREZ <53586202+LazaroPerez0823@users.noreply.github.com>
2019-11-03 22:43:22 -0500

    Final with comments added

--- script/script.js
+++ script/script.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
 
+    // Set Global Variables
+
     var userLat;
     var userLng;
     var destLat;
     var destLng;
     var userRadius;
-    var meters = 804;
+    var meters = 805;
     var results;
-    var i = 0;
-    
+    var i;
+
+    //Sets the side bar to hidden on page view
     $("#sideContainer").hide();
-    //dropdown for states
+
+    //Sets dropdown for states
     var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
         "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
         "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
@@ -18,16 +22,8 @@ $(document).ready(function () {
         "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
         "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
     ];
-    var radius = [0.5, 1, 2, 5, 8, 10, 15, 20, 30, 50];
-    //previous objects array
-    var previousPlaces = [];
-    //close modal on click
-    $("#closeBtn, .button").on("click", function (event) {
-        event.preventDefault();
-        // call outside functions for actions on click
-        $("#addressModal").hide();
-    });
-    //dropdown for states menu
+
+    //Populate State dropdown
     for (var i = 0; i < states.length; i++) {
         var dropDown = $("<option>");
         dropDown.addClass("stateOption");
@@ -35,7 +31,12 @@ $(document).ready(function () {
         dropDown.text(states[i]);
         $("#inputState").append(dropDown);
     }
-    //dropdown for radius menu
+
+
+    //Sets Radius dropdown
+    var radius = [0.5, 1, 2, 5, 8, 10, 15, 20, 30, 50];
+
+    //Populates dropdown for radius menu
     for (var j = 0; j < radius.length; j++) {
         var radDropdown = $("<option>");
         radDropdown.addClass("radius");
@@ -43,10 +44,26 @@ $(document).ready(function () {
         radDropdown.text(radius[j]);
         $("#inputRadius").append(radDropdown);
     }
+
+
+    //previous objects array
+    var previousPlaces = [];
+
+    //Closes user input modal on an button click
+    $("#closeBtn, .button").on("click", function (event) {
+        event.preventDefault();
+        // call outside functions for actions on click
+        $("#addressModal").hide();
+    });
+
+
+
+
     //show side nav
     $("#prevSearches").on("click", function () {
         $("#sideContainer").show();
     });
+
     //hide side nav when clicked outside
     $(document).mouseup(function (i) {
         var sideList = $("#sideNav");
@@ -55,71 +72,7 @@ $(document).ready(function () {
         }
     });
 
-
-    $("#modalFindMeBtn").on("click", function (event) {
-        event.preventDefault()
-        // call outside functions for actions on click
-        findLocation();
-    });
-
-    // When users click "search"
-    $("#modalGoBtn").on('click', function () {
-        // Prevent the page from resetting
-        event.preventDefault();
-        // My location variables:
-        // Address, City, State, Zip,
-        var userAddress = $('#inputAddress').val().trim().split(' ').join('+');
-        var userCity = $('#inputCity').val().trim().split(' ').join('+');
-        var userState = $('#inputState').val().trim();
-        userRadius = $('#inputRadius').val().trim();
-
-        meters = parseInt(userRadius * 1609.344);
-
-        var APIKey = "&key=AIzaSyAE2CIuMnHiuUN7XLs9fRiATGN1gD-t0LY";
-        // Here we are building the URL we need to query the database
-        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userAddress + "," + userCity + "," + userState + APIKey;
-        // We then created an AJAX call
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            userLat = (response.results[0].geometry.location.lat);
-            userLng = (response.results[0].geometry.location.lng);
-            console.log(queryURL)
-            var map, infoWindow;
-            var pos = {
-                lat: userLat,
-                lng: userLng
-
-            };
-            getFoodSpots();
-
-            infoWindow = new google.maps.InfoWindow;
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {
-                    lat: userLat,
-                    lng: userLng,
-                },
-
-                zoom: 15
-            });
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('You.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-            //remove
-            console.log(queryURL)
-        });
-    });
-
-    $("#closeBtn, .button").on("click", function (event) {
-        event.preventDefault();
-        $("#addressModal").hide();
-    })
-    // // MAP FUNCTIONS
-    //Displays map on screen
+    //Sets the inital page display of the map in the background.  Hard Coded a random location into it
     var map, infoWindow;
     infoWindow = new google.maps.InfoWindow;
     map = new google.maps.Map(document.getElementById('map'), {
@@ -129,11 +82,60 @@ $(document).ready(function () {
         },
         zoom: 15
     });
-    //Calls function to location you on the map
+
+    //Clicking the Find a Place Button calls the map location function
+    $("#modalFindMeBtn").on("click", function (event) {
+        event.preventDefault()
+        findLocation();
+    });
+
+    // When users searches for a specified address - runs Geocode API to get Lat and Lng
+    $("#modalGoBtn").on('click', function () {
+        event.preventDefault();
+        var userAddress = $('#inputAddress').val().trim().split(' ').join('+');
+        var userCity = $('#inputCity').val().trim().split(' ').join('+');
+        var userState = $('#inputState').val().trim();
+        //Ajax call info and call
+        var APIKey = "&key=AIzaSyAE2CIuMnHiuUN7XLs9fRiATGN1gD-t0LY";
+        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userAddress + "," + userCity + "," + userState + APIKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            //Gets users Lat and Lng from Address
+            userLat = (response.results[0].geometry.location.lat);
+            userLng = (response.results[0].geometry.location.lng);
+            var map, infoWindow;
+            var pos = {
+                lat: userLat,
+                lng: userLng
+            };
+            //runs the AJAX to get a JSON for the different food spots bassed on the location
+            getFoodSpots();
+
+            //sets map view based on location from address
+            infoWindow = new google.maps.InfoWindow;
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: userLat,
+                    lng: userLng,
+                },
+
+                zoom: 15
+            });
+            //Sets the position and shows the info window on map with 'You' as the message.  Centers the map on that location
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        });
+    });
+
+
+    //Calls function to location you on the map when no address is entered and grabs the user
     function findLocation() {
-        userRadius = $('#inputRadius').val().trim();
-        meters = parseInt(userRadius * 1609.344);
-        // Try HTML5 geolocation.
+
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function (position) {
                 var pos = {
@@ -147,7 +149,7 @@ $(document).ready(function () {
                 userLat = position.coords.latitude;
                 userLng = position.coords.longitude;
 
-
+                //Runs the Zomato ajax call
                 getFoodSpots();
 
             }, function () {
@@ -171,28 +173,26 @@ $(document).ready(function () {
     // This function is run when either locator button is pushed.  
 
     function getFoodSpots() {
-
+        userRadius = $('#inputRadius').val().trim();
+        meters = parseInt(userRadius * 1609.344);
         var zBaseURL = "https://developers.zomato.com/api/v2.1/search?"
         var APIKey = "&apikey=2acf625e70fd25f7205fda31a0f6cb15";
         var queryURL = "https://developers.zomato.com/api/v2.1/search?" + "lat=" + userLat + "&lon=" + userLng + "&" + "radius=" + meters + "&order=asc" + "&sort=rating" + APIKey;
-        console.log(queryURL);
+
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             results = response.restaurants;
-          
+
         });
 
     }
 
-
-    
-
+    //Picks a randomn location from the Zoomato food array and sets it to the side nave as well as calls the destnation map function
     $("#searchAgain").on("click", function () {
         var randomNumber = [Math.floor(Math.random() * 20)];
-        console.log(results[randomNumber]);
         i = randomNumber
         var placeHolder = $('<li>');
         var a = $('<a>');
@@ -219,107 +219,108 @@ $(document).ready(function () {
         $("#sideNav").append(placeHolder);
         destLat = (results[i].restaurant.location.latitude * 1);
         destLng = (results[i].restaurant.location.longitude * 1);
-         destMap();
-
-
+        destMap();
     });
 
-
+    //Sets the second map with the location of the food place and your current location
     function destMap() {
         lats = destLat;
         lngs = destLng;
-    
-        console.log(results);
-        console.log(i);
 
-
-            // Map options
-            var options = {
-              zoom:12,
-              center:{lat:41.955048,lng:-79.835499}
+        // Map options
+        var options = {
+            zoom: 15,
+            center: {
+                lat: 41.955048,
+                lng: -79.835499
             }
-      
-            // New map
-            var map = new google.maps.Map(document.getElementById('map'), options);
-            var rendererOptions = {
-                suppressMarkers: true,
-              };
-            var directionsService = new google.maps.DirectionsService();
-            var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
-            directionsDisplay.setMap(map);
-            var request = {   travelMode: google.maps.TravelMode.DRIVING, optimizeWaypoints: true, waypoints: []  };
-      
-           
-            // Array of markers
-            var markers = [
-              {
-               coords:{lat:userLat,lng:userLng},
-            //   iconImage:'ME.png',
-              content:'You'
-              },
-                {
-               coords:{lat:lats,lng:lngs},
-            //   iconImage:'food.png',
-              content: results[i].restaurant.name
-              }
-            ];
-           
+        }
 
-   
-                       // Loop through markers
-            for(var l = 0; l < markers.length; l++){
-              // Add marker
-              addMarker(markers[l]);
+        //Dest map settings
+        var map = new google.maps.Map(document.getElementById('map'), options);
+        var rendererOptions = {
+            suppressMarkers: true,
+        };
+        var directionsService = new google.maps.DirectionsService();
+        var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+        directionsDisplay.setMap(map);
+        var request = {
+            travelMode: google.maps.TravelMode.DRIVING,
+            optimizeWaypoints: true,
+            waypoints: []
+        };
+
+
+        // Array of markers that show on the map (Start and finish locations)
+        var markers = [{
+                coords: {
+                    lat: userLat,
+                    lng: userLng
+                },
+                //   iconImage:'ME.png',
+                content: 'You'
+            },
+            {
+                coords: {
+                    lat: lats,
+                    lng: lngs
+                },
+                //   iconImage:'food.png',
+                content: results[i].restaurant.name
             }
-      
-            // Add Marker Function
-            function addMarker(props){
-              var marker = new google.maps.Marker({
-                position:props.coords,
-                map:map,
+        ];
+
+        // Loop through markers object to set each
+        for (var l = 0; l < markers.length; l++) {
+            // Add marker
+            addMarker(markers[l]);
+        }
+
+        // Add Marker Function
+        function addMarker(props) {
+            var marker = new google.maps.Marker({
+                position: props.coords,
+                map: map,
                 // icon:props.iconImage
-              });
-      
-                // Check content
-              if(props.content){
+            });
+
+            // Check content
+            if (props.content) {
                 var infoWindow = new google.maps.InfoWindow({
-                  content:props.content
+                    content: props.content
                 });
-      
-                marker.addListener('click', function(){
-                  infoWindow.open(map, marker);
-                  
+
+                marker.addListener('click', function () {
+                    infoWindow.open(map, marker);
+
                 });
-                             }     
-              if (l === 0) { 
-                  request.origin = props.coords; 
-              }
-              else if (l === markers.length - 1) {
-                  request.destination = props.coords;
-                  }
-                  else {
-                      if (props.coords) {
-                      request.waypoints.push({
-                      location: props.coords,
-                      stopover: true
-                          })
-                      }
-      
-                  }
-                  infoWindow.open(map, marker);
-              //End of Add Marker Function
-              }
-          directionsService.route(request,function(response,status){
-              if (status == "OK"){
-                  directionsDisplay.setDirections(response)
-              }
-     
-          });
-          }
-   
-    $(document).on("click", ".link", function() {
+            }
+            if (l === 0) {
+                request.origin = props.coords;
+            } else if (l === markers.length - 1) {
+                request.destination = props.coords;
+            } else {
+                if (props.coords) {
+                    request.waypoints.push({
+                        location: props.coords,
+                        stopover: true
+                    })
+                }
+
+            }
+            infoWindow.open(map, marker);
+            //End of Add Marker Function
+        }
+        directionsService.route(request, function (response, status) {
+            if (status == "OK") {
+                directionsDisplay.setDirections(response)
+            }
+
+        });
+    }
+
+    $(document).on("click", ".link", function () {
         event.preventDefault();
         window.open(this.href, "_blank");
     });
 });
-
